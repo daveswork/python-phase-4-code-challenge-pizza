a634573a2e36d31e6ee57b94739eae4320a30c95
Dave J <29786887+daveswork@users.noreply.github.com>
2024-09-25 13:35:54 -0400

    All local tests pass

--- server/models.py
+++ server/models.py
@@ -21,12 +21,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-    restaurant_pizza = db.relationship("RestaurantPizza", back_populates="restaurant", cascade='all, delete-orphan')
-    pizzas = association_proxy("restaurant_pizza", "pizza")
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant", cascade='all, delete-orphan')
+    # pizzas = association_proxy("restaurant_pizza", "pizza")
 
     # add serialization rules
 
-    serialize_rules = ["-pizzas.restaurant"]
+    serialize_rules = ["-restaurant_pizza.restaurant"]
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -40,12 +40,12 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-    restaurant_pizza = db.relationship("RestaurantPizza", back_populates="pizza", cascade='all, delete-orphan')
-    restaurants = association_proxy("restaurant_pizzas", "restaurant")
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza", cascade='all, delete-orphan')
+    # restaurants = association_proxy("restaurant_pizzas", "restaurant")
 
 
     # add serialization rules
-    serialize_rules = ["-restaurants.pizza"]
+    serialize_rules = ["-restaurant_pizza.pizza"]
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -62,12 +62,12 @@ class RestaurantPizza(db.Model, SerializerMixin):
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
-    pizza = db.relationship("Pizza", back_populates="restaurant_pizza")
-    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizza")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
 
     # add serialization rules
 
-    serialize_rules = ['restaurant.restaurant_pizza', 'pizza.restaurant_pizza']
+    serialize_rules = ['-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas']
 
     # add validation
     @validates('price')
